f188f64f1e758dcda0521c97498d2bf1cbc9fc4d
Roy <yyss9103@gmail.com>
2023-09-21 14:35:46 +0900

    filter 응닫바디 한글 깨짐 이슈 해결

--- src/main/kotlin/com/example/productkotlin/config/AuthFilter.kt
+++ src/main/kotlin/com/example/productkotlin/config/AuthFilter.kt
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.filter.OncePerRequestFilter
 import java.time.LocalDateTime
-import javax.naming.AuthenticationException
 
 class AuthFilter(
     val custSessionRepository: CustSessionRepository,
@@ -20,7 +19,6 @@ class AuthFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-
         val uri = request.requestURI
 
         if (!uri.startsWith("/api/ch")) {
@@ -30,17 +28,33 @@ class AuthFilter(
 
         val requestSessionId = request.getHeader("Authorization")
 
-
         if (requestSessionId == null) {
-            errorHandler(response, ErrorDetailsDto(status = 403, errorMessage = "로그인이 필요한 요청입니다.", timeStamp = LocalDateTime.now().toString()))
+
+            val responseDto = ErrorDetailsDto(
+                status = 403,
+                errorMessage = "로그인이 필요한 요청입니다.",
+                timeStamp = LocalDateTime.now().toString()
+            )
+
+            errorHandler(response, responseDto)
             return
         }
 
         val custSession = custSessionRepository.findById(requestSessionId)
-            .orElseThrow { AuthenticationException("로그인이 필요한 요청입니다.") }
-        //TODO : 여기도 errorHandler 보기
 
-        val currentCust = CurrentCust(custSession.custId)
+        if (custSession.isEmpty) {
+            val responseDto = ErrorDetailsDto(
+                status = 403,
+                errorMessage = "로그인이 필요한 요청입니다.",
+                timeStamp = LocalDateTime.now().toString()
+            )
+
+            errorHandler(response, responseDto)
+            return
+        }
+
+        val currentCust = CurrentCust(custSession.get().custId)
+
         request.setAttribute("currentCust", currentCust)
 
         filterChain.doFilter(request, response)
@@ -51,6 +65,8 @@ class AuthFilter(
         responseDto: ErrorDetailsDto
     ) {
         response.status = responseDto.status
+        response.contentType = "application/json;charset=UTF-8"
+
         response.writer.write(objectMapper.writeValueAsString(responseDto))
     }
 
