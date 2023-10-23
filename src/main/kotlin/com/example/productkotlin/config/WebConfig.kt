a1e829a025db4da6d8c384e70bf588ad8229e8a8
Roy <yyss9103@gmail.com>
2023-10-23 22:55:46 +0900

    Feat: cors 추가
    cors 추가
    
    close #issueNo

--- src/main/kotlin/com/example/productkotlin/config/WebConfig.kt
+++ src/main/kotlin/com/example/productkotlin/config/WebConfig.kt
@@ -1,7 +1,9 @@
 package com.example.productkotlin.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -9,5 +11,17 @@ class WebConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(CustAuthResolver())
         resolvers.add(MemberAuthResolver())
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+            }
+        }
     }
 }
